9bbdbcccc9edb4cf000c49529fdf93d86eb9093b
Patrik Dahlström <patrikd@telia.com>
2012-10-11 18:23:00 +0200

    Added pretty print of binary data to linux port

--- ports/linux.c
+++ ports/linux.c
@@ -25,13 +25,31 @@ static PANCSTATUS linux_read_func(void)
 static PANCSTATUS linux_write_func(void *dev_data, uint8_t *data, uint16_t *length)
 {
 	size_t ret;
+	uint8_t bit;
+	uint16_t i;
+	uint8_t j;
 	FILE *out = (FILE*)dev_data;
 
-	ret = fprintf(out, "%s", (char*)data);
-	if (ret < 0) {
-		goto err_out;
+	fputs("Sending packet to device:\n", out);
+	fputs("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n", out);
+	for (i=0; i < (*length); i++) {
+		fprintf(out, "|");
+		for (j=0; j < 8; j++) {
+			bit = ( (*data) >> (7-j%8) ) & 0x01;
+			fprintf(out, "%i", bit);
+
+			/* Place a space between every bit except last */
+			if (j != 7) {
+				fprintf(out, " ");
+			}
+		}
+		
+		if ( (i+1) % 4 == 0 ) {
+			fputs("|\n", out);
+			fputs("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n", out);
+		}
+		data++;
 	}
-	*length = ret;
 
 	return PANCSTATUS_OK;
 err_out:
