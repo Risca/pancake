23415e4db357feda5320715a8cce4990f17958c9
Jacob Carlsson <jacob.carlsson@gmail.com>
2012-10-30 11:07:24 +0100

    First draft of header compression
    - Compresses header to 2 bytes, then adds 6 bytes in-line.
    - Outputs header to console

--- common/header_compression.c
+++ common/header_compression.c
@@ -36,7 +36,7 @@ PANCSTATUS pancake_compress_header(struct ip6_hdr *hdr, struct pancake_compresse
 
         // Carried in-line
         default:
-            *data &= ~(11 << 0);  // 00                                                   // REMEMBER Carry in-line
+            *data &= ~(11 << 0);  // 00                                                    // REMEMBER Carry in-line
     }
 
     // Go to next byte in compressed header
@@ -45,9 +45,54 @@ PANCSTATUS pancake_compress_header(struct ip6_hdr *hdr, struct pancake_compresse
     // Context Identifier Extension (1)
     *data |= (1 << 7);
 
-    // Source Address Compression
-    *data &= ~(1 << 6);
+    // Source Address Compression SAC
+    *data &= ~(1 << 6); // Set SAC=0
+
+    // Source adress mode (16 bits address carried in line)
+    *data |= (1 << 5);  // Set bit 5=1
+    *data &= ~(1 << 4); // Clear bit 4, -> 10                                              // REMEMBER Carry in-line
+
+    // Multicast Compression (destination is NOT an Multicast address) (M=0)
+    *data &= ~(1 << 3);
+
+    // Destination Address Compression DAC (DAC=0)
+    *data &= ~(1 << 2); // Set DAC=0
+
+    // Destination Address Mode (16 bits address carried in line                           // REMEMBER Carry in-line
+    *data |= (1 << 1);  // Set bit 1=1
+    *data &= ~(1 << 0); // Clear bit 0, -> 10
 
     // 2 Bytes in size
     compressed_hdr->size = 2;
+
+    /*
+     * Data carried in-line
+     */
+    // Next header
+    data++;     // Byte 3
+    *data = hdr->ip6_nxt;
+    compressed_hdr->size += 1;
+
+    // Hop limit
+    data++;
+    *data = hdr->ip6_hlim;
+    compressed_hdr->size += 1;
+
+    //Source address
+    data++;
+    *data = hdr->ip6_src.s6_addr[14];
+    data++;
+    *data = hdr->ip6_src.s6_addr[15];
+    compressed_hdr->size += 2;
+
+    // Destination address
+    data++;
+    *data = hdr->ip6_dst.s6_addr[14];
+    data++;
+    *data = hdr->ip6_dst.s6_addr[15];
+    compressed_hdr->size += 2;
 }
+
+/**
+ *
+ */
